92042c5d769ad406f530db2e3cba179561ce83d4
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2020-04-02 01:07:21 +0600

    On start, cache events too

--- docker/app/docker-entrypoint.sh
+++ docker/app/docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ -z "$1" ]; then
 
     if [ "$env" != "local" ]; then
         echo "Caching configuration..." >> /proc/1/fd/1
-        (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+        (cd /var/www/html && php artisan config:cache && php artisan event:cache && php artisan route:cache && php artisan view:cache)
     fi
 
     if [ "$role" = "app" ]; then
