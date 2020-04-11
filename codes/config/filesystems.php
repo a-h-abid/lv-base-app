4bfcde469fea6e26f0f8bef6c57801f1a86e4a48
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2020-04-11 12:25:23 +0600

    Added Minio Support

--- codes/config/filesystems.php
+++ codes/config/filesystems.php
@@ -65,6 +65,16 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('MINIO_ACCESS_KEY'),
+            'secret' => env('MINIO_SECRET_KEY'),
+            'region' => env('MINIO_REGION'),
+            'bucket' => env('MINIO_BUCKET'),
+            'endpoint' => env('MINIO_ENDPOINT'),
+            'use_path_style_endpoint' => true,
+        ],
+
     ],
 
     /*
